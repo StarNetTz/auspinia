e6679489339aefaac6e72a817fc380692b7f9fcd
Zvjezdan Tomicevic <zeko77@gmail.com>
2016-09-14 12:27:33 +0200

    Implemented i18n
    Locale translation pattern and labels

--- src/components/topBar.js
+++ src/components/topBar.js
@@ -1,4 +1,14 @@
+import { inject } from 'aurelia-framework';
+import {I18N} from 'aurelia-i18n';
+
+@inject(I18N)
 export class TopBar {
-	constructor(){
+	constructor(i18n){
+		this.i18n = i18n;
+	}
+
+	setLocale(lang) {
+  		this.i18n.setLocale(lang);
+  		console.log(this.i18n.getLocale());
 	}
 }
--- src/main.js
+++ src/main.js
@@ -1,19 +1,36 @@
 import 'bootstrap';
 import 'js/plugins/metisMenu/jquery.metisMenu.custom.min.js';
 import 'js/plugins/slimscroll/jquery.slimscroll.min.js';
-$("alo");
+import { I18N } from 'aurelia-i18n';
+import Backend from 'i18next-xhr-backend';
 
 export function configure(aurelia) {
-  aurelia.use
-    .standardConfiguration()
-    .developmentLogging();
+    aurelia.use
+        .standardConfiguration()
+        .developmentLogging()
+        .plugin('aurelia-i18n', (instance) => {
+            // register backend plugin
+            instance.i18next.use(Backend);
 
-  //Uncomment the line below to enable animation.
-  //aurelia.use.plugin('aurelia-animator-css');
-  //if the css animator is enabled, add swap-order="after" to all router-view elements
+            // adapt options to your needs (see http://i18next.com/docs/options/)
+            // make sure to return the promise of the setup method, in order to guarantee proper loading
+            return instance.setup({
+                backend: { // <-- configure backend settings
+                    loadPath: './locale/{{lng}}/{{ns}}.json', // <-- XHR settings for where to get the files from
+                },
+                lng: 'en',
+                attributes: ['t', 'i18n'],
+                fallbackLng: 'en',
+                debug: false
+            });
+        });
 
-  //Anyone wanting to use HTMLImports to load views, will need to install the following plugin.
-  //aurelia.use.plugin('aurelia-html-import-template-loader')
+    //Uncomment the line below to enable animation.
+    //aurelia.use.plugin('aurelia-animator-css');
+    //if the css animator is enabled, add swap-order="after" to all router-view elements
 
-  aurelia.start().then(() => aurelia.setRoot("shell"));
+    //Anyone wanting to use HTMLImports to load views, will need to install the following plugin.
+    //aurelia.use.plugin('aurelia-html-import-template-loader')
+
+    aurelia.start().then(() => aurelia.setRoot("shell"));
 }
