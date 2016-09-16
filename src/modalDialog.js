3e9ff166dee8f331b8fd70c2c76a83265c032843
AlmaMuratovic <alma.muratovic22@gmail.com>
2016-09-16 15:52:30 +0200

    Modal localization

--- src/modalDialog.js
+++ src/modalDialog.js
@@ -1,12 +1,14 @@
 import { inject } from 'aurelia-framework';
 import { DialogController } from 'aurelia-dialog';
+import { I18N } from 'aurelia-i18n';
 
-@inject(DialogController)
+@inject(DialogController, I18N)
 
 export class ModalDialog {
 
-    constructor(controller) {
+    constructor(controller, i18n) {
         this.controller = controller;
+        this.i18n= i18n;
     }
 
     activate(c) {
@@ -24,7 +26,7 @@ export class ModalDialog {
     ok() {
         this.controller.ok();
         swal({
-            title: "Good job!",
+            title: this.i18n="Goodjob",
             text: "ok!",
             type: "success"
         });
