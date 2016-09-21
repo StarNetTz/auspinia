1ca8b34c7324c8e32d2d410121d24b484d042a2a
AlmaMuratovic <alma.muratovic22@gmail.com>
2016-09-21 10:32:35 +0200

    Dashboard v.4 completed

--- src/views/dashboards/dashboard_4.js
+++ src/views/dashboards/dashboard_4.js
@@ -1,4 +1,147 @@
-export class dashboard_4{
-	constructor(){
-	}
+/*Flot*/
+import '../../js/plugins/flot/jquery.flot.js';
+import '../../js/plugins/flot/jquery.flot.tooltip.min.js';
+
+/*ChartJS*/
+import '../../js/plugins/chartJs/Chart.min.js';
+
+/*Peity*/
+import '../../js/plugins/peity/jquery.peity.min.js';
+
+
+
+export class dashboard_4 {
+    constructor() {}
+    initPeity() {
+        $("span.pie").peity("pie", {
+            fill: ['#1ab394', '#d7d7d7', '#ffffff']
+        })
+
+        $(".line").peity("line", {
+            fill: '#1ab394',
+            stroke: '#169c81',
+        })
+
+        $(".bar").peity("bar", {
+            fill: ["#1ab394", "#d7d7d7"]
+        })
+
+        $(".bar_dashboard").peity("bar", {
+            fill: ["#1ab394", "#d7d7d7"],
+            width: 100
+        })
+
+        var updatingChart = $(".updating-chart").peity("line", { fill: '#1ab394', stroke: '#169c81', width: 64 })
+
+        setInterval(function() {
+            var random = Math.round(Math.random() * 10)
+            var values = updatingChart.text().split(",")
+            values.shift()
+            values.push(random)
+
+            updatingChart
+                .text(values.join(","))
+                .change()
+        }, 1000);
+    }
+    initGraph() {
+        var d1 = [
+            [1262304000000, 6],
+            [1264982400000, 3057],
+            [1267401600000, 20434],
+            [1270080000000, 31982],
+            [1272672000000, 26602],
+            [1275350400000, 27826],
+            [1277942400000, 24302],
+            [1280620800000, 24237],
+            [1283299200000, 21004],
+            [1285891200000, 12144],
+            [1288569600000, 10577],
+            [1291161600000, 10295]
+        ];
+        var d2 = [
+            [1262304000000, 5],
+            [1264982400000, 200],
+            [1267401600000, 1605],
+            [1270080000000, 6129],
+            [1272672000000, 11643],
+            [1275350400000, 19055],
+            [1277942400000, 30062],
+            [1280620800000, 39197],
+            [1283299200000, 37000],
+            [1285891200000, 27000],
+            [1288569600000, 21000],
+            [1291161600000, 17000]
+        ];
+
+        var data1 = [
+            { label: "Data 1", data: d1, color: '#17a084' },
+            { label: "Data 2", data: d2, color: '#127e68' }
+        ];
+        $.plot($("#flot-chart1"), data1, {
+            xaxis: {
+                tickDecimals: 0
+            },
+            series: {
+                lines: {
+                    show: true,
+                    fill: true,
+                    fillColor: {
+                        colors: [{
+                            opacity: 1
+                        }, {
+                            opacity: 1
+                        }]
+                    },
+                },
+                points: {
+                    width: 0.1,
+                    show: false
+                },
+            },
+            grid: {
+                show: false,
+                borderWidth: 0
+            },
+            legend: {
+                show: false,
+            }
+        });
+
+        var lineData = {
+            labels: ["January", "February", "March", "April", "May", "June", "July"],
+            datasets: [{
+                label: "Example dataset",
+                backgroundColor: "rgba(26,179,148,0.5)",
+                borderColor: "rgba(26,179,148,0.7)",
+                pointBackgroundColor: "rgba(26,179,148,1)",
+                pointBorderColor: "#fff",
+                data: [48, 48, 60, 39, 56, 37, 30]
+            }, {
+                label: "Example dataset",
+                backgroundColor: "rgba(220,220,220,0.5)",
+                borderColor: "rgba(220,220,220,1)",
+                pointBackgroundColor: "rgba(220,220,220,1)",
+                pointBorderColor: "#fff",
+                data: [65, 59, 40, 51, 36, 25, 40]
+            }]
+        };
+
+        var lineOptions = {
+            responsive: true
+        };
+
+
+        var ctx = document.getElementById("lineChart").getContext("2d");
+        new Chart(ctx, { type: 'line', data: lineData, options: lineOptions });
+
+    }
+
+    attached() {
+        this.initPeity();
+        this.initGraph();
+
+
+
+    }
 }
