f3c256b2bb24affea7d31475f64169c014f769d8
vedao <vsijercic@gmail.com>
2016-09-23 15:56:47 +0200

    Forms part 2

---
+++ src/views/forms/advancedPlugins.js
@@ -0,0 +1,15 @@
+//import '../../js/plugins/chosen/chosen.jquery.js';
+//import '../../js/plugins/jsKnob/jquery.knob.js';
+//import '../../js/plugins/jasny/jasny-bootstrap.min.js';
+//import '../../js/plugins/datapicker/bootstrap-datepicker.js';
+//import '../../js/plugins/nouslider/jquery.nouislider.min.js';
+//import '../../js/plugins/switchery/switchery.js';
+//import '../../js/plugins/colorpicker/bootstrap-colorpicker.min.js';
+//import '../../js/plugins/clockpicker/clockpicker.js';
+
+
+export class layouts{
+	constructor(){
+	
+	}
+}
